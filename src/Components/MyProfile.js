63b773c2dca58f9d47c306f9825dd94366fad04e
erobbi <elirobbins0@gmail.com>
2021-09-30 13:31:55 -0400

    added username updater

--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -1,86 +1,123 @@
 import React, { useState } from "react";
 
 export default function MyProfile({ user, setUser }) {
-    const [showNameUpdater, setShowNameUpdater] = useState(false);
-    const [showAvatarUpdater, setShowAvatarUpdater] = useState(false);
+  const [showNameUpdater, setShowNameUpdater] = useState(false);
+  const [showUsernameUpdater, setShowUsernameUpdater] = useState(false);
+  const [showAvatarUpdater, setShowAvatarUpdater] = useState(false);
 
-    function NameUpdater() {
-        const [name, setName] = useState(user.name)
-        function handleSubmit(e) {
-            e.preventDefault();
-            console.log(user)
-            fetch(`users/1`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name
-                }),
-            })
-            .then((res) => res.json())
-            .then((user) => {
-                setUser(user)
-                setShowNameUpdater(false)
-            })
-        }
-
-        return (
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text"
-                    placeholder={name}
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                />
-                <input type="submit" value="Submit"/>
-            </form>
-        )
+  function NameUpdater() {
+    const [name, setName] = useState(user.name);
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(user);
+      fetch(`users/1`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+        }),
+      })
+        .then((res) => res.json())
+        .then((user) => {
+          setUser(user);
+          setShowNameUpdater(false);
+        });
     }
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={name}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 
-    function AvatarUpdater() {
-      const [avatar_url, setAvatar_url] = useState(user.avatar_url);
-      function handleSubmit(e) {
-        e.preventDefault();
-        console.log(user);
-        fetch(`users/1`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            avatar_url,
-          }),
-        })
-          .then((res) => res.json())
-          .then((user) => {
-            setUser(user);
-            setShowAvatarUpdater(false);
-          });
-      }
-
-      return (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder={avatar_url}
-            value={avatar_url}
-            onChange={(e) => setAvatar_url(e.target.value)}
-          />
-          <input type="submit" value="Submit" />
-        </form>
-      );
+  function UsernameUpdater() {
+    const [username, setUsername] = useState(user.username);
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(user);
+      fetch(`users/1`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+        }),
+      })
+        .then((res) => res.json())
+        .then((user) => {
+          setUser(user);
+          setShowUsernameUpdater(false);
+        });
     }
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={username}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
+  function AvatarUpdater() {
+    const [avatar_url, setAvatar_url] = useState(user.avatar_url);
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(user);
+      fetch(`users/1`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          avatar_url,
+        }),
+      })
+        .then((res) => res.json())
+        .then((user) => {
+          setUser(user);
+          setShowAvatarUpdater(false);
+        });
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder={avatar_url}
+          value={avatar_url}
+          onChange={(e) => setAvatar_url(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
   return (
     <div>
       {user ? (
         <>
-          <div>Profile Updater</div>
           <h2>Name: {user.name}</h2>
           <button onClick={() => setShowNameUpdater(!showNameUpdater)}>
             Update Name
           </button>
           {showNameUpdater ? <NameUpdater /> : null}
+          <h2>Username: {user.username}</h2>
+          <button onClick={() => setShowUsernameUpdater(!showUsernameUpdater)}>
+            Update Username
+          </button>
+          {showUsernameUpdater ? <UsernameUpdater /> : null}
           <h3>Avatar:</h3>
           {user.avatar_url ? (
             <img src={user.avatar_url} alt="avatar" className="avatar" />
