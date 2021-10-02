fac3c4004f215d11ddeaf55f77cb90efd785d301
erobbi <elirobbins0@gmail.com>
2021-10-01 22:11:53 -0400

    added initial weight to NewPetForm

--- src/Components/NewPetForm.js
+++ src/Components/NewPetForm.js
@@ -10,6 +10,7 @@ export default function NewPetForm() {
     const ages = Array.from(Array(31).keys())
     ages.shift()
     const [errors, setErrors] = useState([]);
+    const [weight, setWeight] = useState("");
     const history = useHistory();
 
     function handleSubmit(e){
@@ -30,6 +31,25 @@ export default function NewPetForm() {
             if (r.ok) {
                 r.json().then((cat)=>{
                     history.push("/weight_tracker")
+                    console.log(cat)
+                    const cat_id=cat.id
+                        fetch("/weights", {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          body: JSON.stringify({
+                            weight,
+                            cat_id,
+                          }),
+                        }).then((res) => {
+                          if (res.ok) {
+                            res.json().then((data) => {
+                            });
+                          } else {
+                            res.json().then((err) => setErrors(err.errors));
+                          }
+                        });
                 })
             } else {
                 r.json().then((err) => setErrors(err.errors))
@@ -79,6 +99,15 @@ export default function NewPetForm() {
             id="food_per_day"
             value={food_per_day}
             onChange={(e) => setFood_per_day(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Weight (in lbs)"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
           />
           <br />
           <br />
