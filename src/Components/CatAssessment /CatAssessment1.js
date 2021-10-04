c6fbf3871a5a4729eb18231fde93c53e7de96526
erobbi <elirobbins0@gmail.com>
2021-10-04 14:04:27 -0400

    survey 1 finished

--- src/Components/CatAssessment /CatAssessment1.js
+++ src/Components/CatAssessment /CatAssessment1.js
@@ -1,16 +1,35 @@
 import React from "react";
+import maxcat from "../../Images/max_cat_transparent.png";
 
 export default function CatAssessment1() {
   return (
     <div>
-      <div className="survey">
-        <div className="survey_content">
-          <div>Let's Get Started</div>
+      <div className="orangeBackground">
+        <div className="survey">
+          <div className="survey_content">
+            <div className="survey_content_inner">
+              <div className="survey_content_main">
+                <img src={maxcat} className="maxcat" />
+                <h1 className="survey_headers">
+                  <span>
+                    Welcome to the new <em>Whisker Watchers</em> assessment.
+                  </span>
+                  <br />
+                  <span>Let's get to know your cat.</span>
+                </h1>
+              </div>
+            </div>
+          </div>
         </div>
+        <footer className="survey_footer">
+          <div className="survey_button_box">
+            <div className="survey_button_box_inner">
+              <button className="primary_button">Next</button>
+              <a class="skip_survey_button">I'm ready to sign up</a>
+            </div>
+          </div>
+        </footer>
       </div>
-      <footer className="survery_footer">
-          <div>hello</div>
-      </footer>
     </div>
   );
 }
