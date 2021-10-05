8a356ae74e591b64fdeb7132f53f7e447e6bf05b
erobbi <elirobbins0@gmail.com>
2021-10-05 12:26:51 -0400

    more style work on survey

--- src/Components/CatSurvey/CatSurvey1.js
+++ src/Components/CatSurvey/CatSurvey1.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import maxcat from "../../Images/max_portrait.png";
 
 export default function CatSurvey1() {
@@ -22,13 +22,15 @@ export default function CatSurvey1() {
             </div>
           </div>
         </div>
-        <footer className="survey_footer">
-          <div className="survey_button_box">
-            <div className="survey_button_box_inner">
-              <Link to="/catsurvey/2">
-                <button className="primary_button">Next</button>
-              </Link>
-              <a class="skip_survey_button">I'm ready to sign up</a>
+        <footer className="survey1_footer_background">
+          <div className="survey_footer">
+            <div className="survey_button_box">
+              <div className="survey_button_box_inner">
+                <Link to="/catsurvey/2">
+                  <button className="primary_button">Next</button>
+                </Link>
+                <a class="skip_survey_button">I'm ready to sign up</a>
+              </div>
             </div>
           </div>
         </footer>
--- src/Components/CatSurvey/CatSurvey2.js
+++ src/Components/CatSurvey/CatSurvey2.js
@@ -1,49 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom"
 
 export default function CatSurvey2() {
-    const [isNeutered, setIsNeutered] = useState("");
-
+  const [isNeutered, setIsNeutered] = useState("");
 
   return (
     <div>
       <div className="lightOrangeBackground">
         <div className="survey2">
           <div className="surveyNav">
-            <div>
-              <button>Back</button>
-            </div>
+            <Link to='/catsurvey/1'>
+              <button className="secondary_button_disabled">Back</button>
+            </Link>
             <div className="questionNum">Question 1 of 3</div>
           </div>
           <div claName="questionBox">
             <span class="questions">Is your cat spayed/neutered?</span>
           </div>
-          <div classname="options">
-            <input
-              className="inputStyle"
-              type="radio"
-              value="true"
-              name="neutered"
-              onChange={(e) => setIsNeutered(true)}
-            />
-            <label for="true" className="labelStyle">
+          <div classname="standardFlexBox">
+            <label className="labelStyle">
               Yes
+              <input
+                id="1"
+                className="inputStyle"
+                type="radio"
+                value="true"
+                name="neutered"
+                onChange={(e) => setIsNeutered(true)}
+              />
             </label>
-            <input
-              className="inputStyle"
-              type="radio"
-              value="false"
-              name="neutered"
-              onChange={(e) => setIsNeutered(false)}
-            />
-            <label for="false" className="labelStyle">
+            <label className="labelStyle">
               No
+              <input
+                id="2"
+                className="inputStyle"
+                type="radio"
+                value="false"
+                name="neutered"
+                onChange={(e) => setIsNeutered(false)}
+              />
             </label>
             <br />
             <br />
           </div>
+          <div className='survey_footer'>
+
+          <div className='survey_button_box'>
           <div className="survey_button_box_inner">
             <button className="primary_button">Next</button>
             <a class="skip_survey_button">I'm ready to sign up</a>
+          </div>
+          </div>
           </div>
         </div>
       </div>
